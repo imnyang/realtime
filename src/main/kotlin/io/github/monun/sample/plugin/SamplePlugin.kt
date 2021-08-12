bf3621fada7ebe17de6e45c120edea680eb86f52
Monun <monun1010@gmail.com>
2021-08-13 01:58:54 +0900

    kommand 코드 제거

--- src/main/kotlin/io/github/monun/sample/plugin/SamplePlugin.kt
+++ src/main/kotlin/io/github/monun/sample/plugin/SamplePlugin.kt
@@ -1,22 +1,10 @@
 package io.github.monun.sample.plugin
 
-import io.github.monun.kommand.kommand
-import net.kyori.adventure.text.Component.text
 import org.bukkit.plugin.java.JavaPlugin
 
 /**
  * @author Monun
  */
 class SamplePlugin : JavaPlugin() {
-    override fun onEnable() {
-        kommand {
-            register("sample") {
-                then("ping") {
-                    executes {
-                        sender.sendMessage(text("pong"))
-                    }
-                }
-            }
-        }
-    }
+
 }
